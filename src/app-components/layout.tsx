53f19130dd321cd3286940e7b937def6f178bdce
Tyson <tyson@bitcine.com>
2019-04-25 16:31:15 -0400

    wip

--- src/app-components/layout.tsx
+++ src/app-components/layout.tsx
@@ -20,20 +20,20 @@ const Layout: React.FunctionComponent<Props> = ({ children, hideHeader, title, d
 			meta={[
 				{ name: "description", content: description },
 				{ name: "keywords", content: "Swing Analysis, Golf Swing, Golf Swing Analysis, Free Golf Swing Analysis" },
-				{ name: "og:url", content: social },
+				{ name: "og:url", content: "https://analyze.golf" + social },
 				{ name: "og:type", content: "website" },
 				{ name: "og:title", content: "Analyze.Golf | Free Browser-Based Golf Swing Analysis Tool" },
 				{ name: "og:description", content: description },
-				{ name: "og:image", content: social },
-				{ name: "og:image:secure_url", content: social },
-				{ name: "og:image:type", content: "image/jpeg" },
+				{ name: "og:image", content: "https://analyze.golf" + social },
+				{ name: "og:image:secure_url", content: "https://analyze.golf" + social },
+				{ name: "og:image:type", content: "image/jpg" },
 				{ name: "og:site", content: "@analyze.golf" },
 				{ name: "og:creator", content: "@analyze.golf" },
 				{ name: "twitter:card", content: "summary_large_image" },
 				{ name: "twitter:site", content: "@analyze.golf" },
 				{ name: "twitter:title", content: title },
 				{ name: "twitter:description", content: description },
-				{ name: "twitter:image", content: social }
+				{ name: "twitter:image", content: "https://analyze.golf" + social }
 			]}
 		/>
 		{!hideHeader && <Header />}
