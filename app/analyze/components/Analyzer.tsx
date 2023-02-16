b8fe3102b7d45254cc9b883d13436725af1aeda4
Tyson Louth <tysonlouth@gmail.com>
2023-02-16 08:53:05 -0500

    wip

--- app/analyze/components/Analyzer.tsx
+++ app/analyze/components/Analyzer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useSearchParams, redirect } from "next/navigation";
+// import { useSearchParams, redirect } from "next/navigation";
 import { createContext, useCallback, useContext, useState } from "react";
 
 import PlayPause from "./PlayPause";
