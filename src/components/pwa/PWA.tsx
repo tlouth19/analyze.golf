f9db089506bd7c5f9cd4600ed0ec8390f58c6bf0
Tyson Louth <tysonlouth@gmail.com>
2023-03-01 11:30:24 -0500

    Added toast and alert styles

--- src/components/pwa/PWA.tsx
+++ src/components/pwa/PWA.tsx
@@ -1,5 +1,6 @@
-import * as AlertDialog from "@radix-ui/react-alert-dialog";
 import { BsArrowClockwise } from "react-icons/bs";
+import * as AlertDialog from "@radix-ui/react-alert-dialog";
+import * as Toast from "@radix-ui/react-toast";
 
 import { useRegisterSW } from "virtual:pwa-register/react";
 
@@ -26,49 +27,44 @@ const PWA = () => {
     void updateServiceWorker(true);
   };
 
-  const isOpen = offlineReady || needRefresh;
-
   return (
-    <AlertDialog.Root open={isOpen}>
-      <AlertDialog.Portal>
-        <AlertDialog.Overlay className="dark:bg-black bg-white !bg-opacity-90 fixed inset-0" />
-        <AlertDialog.Content className="dark:bg-black bg-white border dark:border-gray-800 rounded shadow-md fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[90vw] max-w-[500px] p-4 max-h-[85vh] focus:outline-none">
-          <AlertDialog.Title className="mb-1 font-bold text-lg uppercase">
-            App Update
-          </AlertDialog.Title>
-          <AlertDialog.Description className="text-current mb-4">
-            {offlineReady ? (
-              <span>App ready to work offline</span>
-            ) : (
-              <span>
-                New content available, click on reload button to update.
-              </span>
-            )}
-          </AlertDialog.Description>
-          <div className="flex items-center justify-end">
-            <AlertDialog.Cancel asChild>
-              <button
-                className="bg-gray-900 p-2 rounded font-bold uppercase text-white"
-                onClick={handleClose}
-              >
-                Close
-              </button>
-            </AlertDialog.Cancel>
+    <>
+      <AlertDialog.Root open={needRefresh}>
+        <AlertDialog.Portal>
+          <AlertDialog.Overlay className="alert-overlay" />
+          <AlertDialog.Content className="alert-content">
+            <AlertDialog.Title className="alert-title">
+              App Update
+            </AlertDialog.Title>
+            <AlertDialog.Description className="alert-description">
+              New content available, click on reload button to update.
+            </AlertDialog.Description>
             <AlertDialog.Action asChild>
-              {needRefresh && (
-                <button
-                  className="ml-2 bg-brand-blue p-2 rounded font-bold uppercase inline-flex gap-1 items-center text-white"
-                  onClick={handleReload}
-                >
-                  <BsArrowClockwise />
-                  Reload App
-                </button>
-              )}
+              <button className="alert-action-btn" onClick={handleReload}>
+                <BsArrowClockwise />
+                Reload App
+              </button>
             </AlertDialog.Action>
-          </div>
-        </AlertDialog.Content>
-      </AlertDialog.Portal>
-    </AlertDialog.Root>
+          </AlertDialog.Content>
+        </AlertDialog.Portal>
+      </AlertDialog.Root>
+      <Toast.Provider swipeDirection="right">
+        <Toast.Root
+          className="toast-root"
+          open={offlineReady}
+          onOpenChange={handleClose}
+        >
+          <Toast.Title className="toast-title">App Update</Toast.Title>
+          <Toast.Description asChild className="toast-description">
+            <span>App is ready to work offline</span>
+          </Toast.Description>
+          <Toast.Action className="toast-action" asChild altText="Close">
+            <button className="toast-btn">Close</button>
+          </Toast.Action>
+        </Toast.Root>
+        <Toast.Viewport className="toast-overlay" />
+      </Toast.Provider>
+    </>
   );
 };
 
