097328701db277fca7528fe7ea868b29111ea5a4
Tyson Louth <tysonlouth@gmail.com>
2023-03-02 15:56:21 -0500

    Fix offline message

--- src/components/progressiveWebApp/ProgressiveWebApp.tsx
+++ src/components/progressiveWebApp/ProgressiveWebApp.tsx
@@ -1,4 +1,4 @@
-import { BsArrowClockwise, BsWifiOff } from "react-icons/bs";
+import { BsArrowClockwise, BsWifiOff, BsXLg } from "react-icons/bs";
 import * as AlertDialog from "@radix-ui/react-alert-dialog";
 import * as Toast from "@radix-ui/react-toast";
 
@@ -29,7 +29,7 @@ const ProgressiveWebApp = () => {
       <AlertDialog.Root open={needRefresh}>
         <AlertDialog.Portal>
           <AlertDialog.Overlay className="dark:bg-black bg-white !bg-opacity-80 data-[state=open]:animate-overlayShow fixed inset-0" />
-          <AlertDialog.Content className="data-[state=open]:animate-contentShow dark:bg-gray-900 bg-gray-100 border dark:border-gray-600 border-gray-300 fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded  p-4 shadow focus:outline-none">
+          <AlertDialog.Content className="data-[state=open]:animate-contentShow dark:bg-black bg-white border dark:border-gray-600 border-gray-300 fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded  p-4 shadow focus:outline-none">
             <AlertDialog.Title className="font-bold text-lg uppercase">
               App Update Available
             </AlertDialog.Title>
@@ -51,24 +51,26 @@ const ProgressiveWebApp = () => {
       </AlertDialog.Root>
       <Toast.Provider swipeDirection="right">
         <Toast.Root
-          className="dark:bg-gray-900 bg-gray-100 border dark:border-gray-600 border-gray-300 rounded shadow p-4 grid [grid-template-areas:_'title_action'_'description_action'] grid-cols-[auto_max-content] gap-x-4 items-center data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut"
+          className="dark:bg-black bg-white border dark:border-gray-600 border-gray-300 rounded shadow p-4 items-center data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut"
           open={offlineReady}
           onOpenChange={handleClose}
         >
-          <Toast.Title className="[grid-area:_title] font-bold text-sm uppercase flex items-center gap-2 mb-1">
-            <BsWifiOff /> Offline Available
-          </Toast.Title>
+          <div className="flex items-center gap-2 justify-between mb-2">
+            <Toast.Title className="font-bold uppercase flex items-center gap-2">
+              <BsWifiOff /> Offline Available
+            </Toast.Title>
+            <Toast.Action className="" asChild altText="Close">
+              <button className="font-semibold">
+                <BsXLg />
+              </button>
+            </Toast.Action>
+          </div>
           <Toast.Description asChild>
-            <span className="text-current text-sm">
+            <p className="text-current text-sm w-full">
               If you&apos;re on the golf course with no connection, the app will
               still work!
-            </span>
+            </p>
           </Toast.Description>
-          <Toast.Action className="[grid-area:_action]" asChild altText="Close">
-            <button className="py-1 px-2 rounded font-semibold uppercase inline-flex gap-1 items-center border-2 border-dashed border-brand-blue text-sm ">
-              Close
-            </button>
-          </Toast.Action>
         </Toast.Root>
         <Toast.Viewport className="fixed bottom-0 right-0 flex flex-col p-6 gap-4 w-[520px] max-w-[100vw] m-0 list-none z-[2147483647] outline-none" />
       </Toast.Provider>
