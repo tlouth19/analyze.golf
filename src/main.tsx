7d2dd24913a97f2f18f16f2547435bacdb231322
Tyson Louth <tysonlouth@gmail.com>
2023-03-03 11:11:25 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import store from "@redux/store";
 import App from "@components/App";
 import ProgressiveWebApp from "@components/ProgressiveWebApp";
-import ErrorBoundary from "@components/errorBoundary/ErrorBoundary";
+import ErrorBoundary from "@components/ErrorBoundary";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
