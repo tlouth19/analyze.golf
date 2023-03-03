1c2238dc9294bba0682d09c294c9603491c5439b
Tyson Louth <tysonlouth@gmail.com>
2023-03-03 12:32:35 -0500

    Fix accessibility

--- src/components/ProgressiveWebApp.tsx
+++ src/components/ProgressiveWebApp.tsx
@@ -49,31 +49,33 @@ const ProgressiveWebApp = () => {
           </AlertDialog.Content>
         </AlertDialog.Portal>
       </AlertDialog.Root>
-      <Toast.Provider swipeDirection="right">
-        <Toast.Root
-          className="dark:bg-black bg-white border dark:border-gray-600 border-gray-300 rounded shadow p-4 items-center data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut"
-          open={offlineReady}
-          onOpenChange={handleClose}
-        >
-          <div className="flex items-center gap-2 justify-between mb-2">
-            <Toast.Title className="font-bold uppercase flex items-center gap-2">
-              <BsWifiOff /> Offline Available
-            </Toast.Title>
-            <Toast.Action className="" asChild altText="Close">
-              <button className="font-semibold">
-                <BsXLg />
-              </button>
-            </Toast.Action>
-          </div>
-          <Toast.Description asChild>
-            <p className="text-current text-sm w-full">
-              If you&apos;re on the golf course with no connection, the app will
-              still work!
-            </p>
-          </Toast.Description>
-        </Toast.Root>
-        <Toast.Viewport className="fixed bottom-0 right-0 flex flex-col p-6 gap-4 w-[520px] max-w-[100vw] m-0 list-none z-[2147483647] outline-none" />
-      </Toast.Provider>
+      {offlineReady && (
+        <Toast.Provider swipeDirection="right">
+          <Toast.Root
+            className="dark:bg-black bg-white border dark:border-gray-600 border-gray-300 rounded shadow p-4 items-center data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut"
+            open={offlineReady}
+            onOpenChange={handleClose}
+          >
+            <div className="flex items-center gap-2 justify-between mb-2">
+              <Toast.Title className="font-bold uppercase flex items-center gap-2">
+                <BsWifiOff /> Offline Available
+              </Toast.Title>
+              <Toast.Action className="" asChild altText="Close">
+                <button className="font-semibold" aria-label="Close">
+                  <BsXLg />
+                </button>
+              </Toast.Action>
+            </div>
+            <Toast.Description asChild>
+              <p className="text-current text-sm w-full">
+                If you&apos;re on the golf course with no connection, the app
+                will still work!
+              </p>
+            </Toast.Description>
+          </Toast.Root>
+          <Toast.Viewport className="fixed bottom-0 right-0 flex flex-col p-6 gap-4 w-[520px] max-w-[100vw] m-0 list-none z-[2147483647] outline-none" />
+        </Toast.Provider>
+      )}
     </>
   );
 };
